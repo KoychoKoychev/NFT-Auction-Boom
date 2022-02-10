d8b7ac6a0c47b58ac10b58ad3d3e3f2034ba28c7
KoychoKoychev <kgkoychev@gmail.com>
2022-02-10 17:41:31 +0200

    fix: try to double up the class names

--- src/components/product/ProductInfo.jsx
+++ src/components/product/ProductInfo.jsx
@@ -9,25 +9,25 @@ import ProductInfoCreator from "./ProductInfoCreator";
 import ProductInfoTimer from "./ProductInfoTimer";
 
 export default function ProductInfo({ title, creator, price, currency, likes, onTimeEnd, timeEnd, isLive }) {
-    
+
     return (
-        <div className={classNames(styles["product-info"])}>
+        <div className={classNames(styles["product-info"], "product-info")}>
             <div className={classNames(styles.product_title)}>
                 <ProductInfoTitle text={title} />
             </div>
             <div className={classNames(styles.product_stats)}>
-            <ProductInfoPrice amount={price} currency={currency} className={classNames(styles.price)}/>
-            <Stack direction="row" spacing={1} className={classNames(styles.stats)}>
-                {isLive?<ProductInfoStatus/>:null}
-                <ProductInfoLikes amount={likes}/>
-            </Stack>
+                <ProductInfoPrice amount={price} currency={currency} className={classNames(styles.price)} />
+                <Stack direction="row" spacing={1} className={classNames(styles.stats)}>
+                    {isLive ? <ProductInfoStatus /> : null}
+                    <ProductInfoLikes amount={likes} />
+                </Stack>
             </div>
             <Grid container columnGap={2} alignItems="flex-end" className={classNames(styles.product_grid)}>
                 <Grid item xs={7}>
-                    <ProductInfoCreator name={creator}/>
+                    <ProductInfoCreator name={creator} />
                 </Grid>
                 <Grid item xs>
-                    <ProductInfoTimer onTimeEnd={onTimeEnd} timeEnd={timeEnd}/>
+                    <ProductInfoTimer onTimeEnd={onTimeEnd} timeEnd={timeEnd} />
                 </Grid>
             </Grid>
         </div>
