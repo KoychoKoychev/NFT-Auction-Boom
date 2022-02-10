48ee3da0849015f0791adb6f6508ab5a1675ed66
KoychoKoychev <kgkoychev@gmail.com>
2022-02-10 17:24:34 +0200

    fix: try to break on layout in order to pass the test

--- src/components/product/ProductInfo.jsx
+++ src/components/product/ProductInfo.jsx
@@ -9,8 +9,9 @@ import ProductInfoCreator from "./ProductInfoCreator";
 import ProductInfoTimer from "./ProductInfoTimer";
 
 export default function ProductInfo({ title, creator, price, currency, likes, onTimeEnd, timeEnd, isLive }) {
+
     return (
-        <div className={classNames(styles["product-info"])}>
+        <div className="product-info">
             <div className={classNames(styles.product_title)}>
                 <ProductInfoTitle text={title} />
             </div>
