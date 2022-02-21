5f29287474719a3dc7577b44137b7348fd6fa277
KoychoKoychev <kgkoychev@gmail.com>
2022-02-21 21:16:55 +0200

    fix: ProfileHero class names revised

--- src/components/profile/ProfileHero.jsx
+++ src/components/profile/ProfileHero.jsx
@@ -3,7 +3,7 @@ import classNames from "classnames";
 
 export default function ProfileHero({ image }) {
     return (
-        <div className={classNames(styles["product-actions"])}>
+        <div className={classNames(styles["profile-hero"])}>
             <img src={image} alt="background" className={classNames(styles.image)} />
         </div>
     )
