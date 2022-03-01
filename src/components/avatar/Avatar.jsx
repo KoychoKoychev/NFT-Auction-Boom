fcadd84cd26a0fe768e4f233afb8ab4ecaf60ee5
KoychoKoychev <kgkoychev@gmail.com>
2022-03-01 10:08:21 +0200

    fix: Avatar badge visibility amended

--- src/components/avatar/Avatar.jsx
+++ src/components/avatar/Avatar.jsx
@@ -3,18 +3,18 @@ import classNames from "classnames";
 
 
 
-export default function Avatar({size=90,url,verified=false}) {
-    return(
+export default function Avatar({ size = 90, url, verified = false }) {
+    return (
         <div className={classNames(styles.avatar)}
-        style={{
-            width:`${size}px`,
-            height:`${size}px`,
-        }}>
-            <img src={url} alt="Avatar image" className={classNames(styles.image)} />
-            <img src="/images/verified.svg" alt="verified icon" className={classNames(styles.badge)}
             style={{
-                display:verified?"block":"none"
-            }}/>
+                width: `${size}px`,
+                height: `${size}px`,
+            }}>
+            <img src={url} alt="Avatar image" className={classNames(styles.image)} />
+            {verified ?
+                <img src="/images/verified.svg" alt="verified icon" className={classNames(styles.badge)}/>
+                :null
+            }
         </div>
     )
 }
