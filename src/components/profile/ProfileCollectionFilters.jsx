cfb297074975d632e825b05f158ebb38b43c5575
KoychoKoychev <kgkoychev@gmail.com>
2022-03-10 10:36:42 +0200

    fix: fully remove the functionality of the filters component

--- src/components/profile/ProfileCollectionFilters.jsx
+++ src/components/profile/ProfileCollectionFilters.jsx
@@ -6,7 +6,7 @@ import { Search } from "@mui/icons-material";
 export default function ProfileCollectionFilters({ filters }) {
     return (
         <div className={classNames(styles["profile-collection-filters"])}>
-            <Stack direction="row" spacing={2}>
+            {/* <Stack direction="row" spacing={2}>
                 <FormControl >
                     <InputLabel id="select_sort_label">Sort by</InputLabel>
                     <Select
@@ -37,14 +37,14 @@ export default function ProfileCollectionFilters({ filters }) {
                         })}
                     </Select>
                 </FormControl>
-                {/* <TextField
+                <TextField
                     className={classNames(styles.text_input)}
                     InputProps={{
                         startAdornment: <InputAdornment position="start"><Search color="primary"/></InputAdornment>
                     }}
                     variant="standard"
-                /> */}
-            </Stack>
+                />
+            </Stack> */}
         </div >
     )
 }
